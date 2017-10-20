10e40fed709e9987d6003e9043160b9bd025c174
puppet <puppet@localhost.local>
2017-10-20 01:32:04 +0000

    Update 'site/profile/manifests/platform/baseline/execdcs.pp'

--- site/profile/manifests/platform/baseline/execdcs.pp
+++ site/profile/manifests/platform/baseline/execdcs.pp
@@ -6,5 +6,11 @@ dsc_windowsfeature{ 'iis':
   dsc_name      => 'Web-Server',
 }
 
+dsc_file{ 'ExampleFile':
+  dcs_ensure          => 'Present',
+  dcs_type            => 'File',
+  dcs_destinationpath => 'C:/westrock.txt',
+  dsc_contents        => 'Custom text file created with puppet dcs module',
+}
 
 }
