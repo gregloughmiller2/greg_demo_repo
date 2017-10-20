6142327851d399348ff39540694d3565d0021f3d
puppet <puppet@localhost.local>
2017-10-20 01:14:36 +0000

    Update 'site/profile/manifests/platform/baseline/execdcs.pp'

--- site/profile/manifests/platform/baseline/execdcs.pp
+++ site/profile/manifests/platform/baseline/execdcs.pp
@@ -1,8 +1,15 @@
 class profile::platform::baseline::execdcs {
 #
 
-dsc_windowsfeature {'featureexample':
-  dsc_ensure    = 'absent'
-  dsc_name      = 'Web-Server'
+dsc_windowsfeature { 'featureexample':
+  dsc_ensure    => 'absent'
+  dsc_name      => 'Web-Server'
+  }
+
+dcs_file { 'ExampleFile':
+  dcs_ensure          => 'present',
+  dcs_type            => 'File',
+  dcs_destinationpath => 'C:/westrock.txt',
+  dsc_contents        =>  'Custome text file created with puppet dcs module',
   }
 }
