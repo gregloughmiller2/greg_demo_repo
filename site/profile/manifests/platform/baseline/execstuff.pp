233be3815075fd20280e0da6b6226eebf39d4e0f
puppet <puppet@localhost.local>
2017-10-20 00:06:49 +0000

    Update 'site/profile/manifests/platform/baseline/execstuff.pp'

--- site/profile/manifests/platform/baseline/execstuff.pp
+++ site/profile/manifests/platform/baseline/execstuff.pp
@@ -1,13 +1,9 @@
 class profile::platform::baseline::execstuff {
 #
 
-registry_key { 'HKLM\System\CurrentControlSet\Services\Puppet':
-    ensure => present,
-}
-registry_value { 'HKLM\System\CurrentControlSet\Services\Puppet\Description':
-  ensure => present,
-  type   => string,
-  data   => "The Puppet Agent service periodically manages your configuration",
-}
+exec { 'custom_fact':
+  command => 'fact_powershell.ps1'
+  path    => 'C:\ProgramData\PuppetLabs\puppet\cache\facts.d'
 
+  }
 }
