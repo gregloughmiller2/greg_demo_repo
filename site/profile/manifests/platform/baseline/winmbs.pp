a4fc6af8f9239138485fc5e7844a847f11485f32
Greg Loughmiller <greg.loughmiller@greg.loughmiller-C02M54SNFD57>
2017-10-22 18:37:59 -0400

    parameters v10

--- site/profile/manifests/platform/baseline/winmbs.pp
+++ site/profile/manifests/platform/baseline/winmbs.pp
@@ -1,6 +1,6 @@
 class profile::platform::baseline::winmbs {
   class { '::profile::platform::baseline::winmbs::winbasesec':
-    mymessage => lookup('::profile::platform::baseline::winmbs::winbasesec:mymessage')
+    mymessage => lookup('profile::platform::baseline::winmbs::winbasesec:mymessage')
   }
 ###  include ::profile::platform::baseline::winmbs::winbasesec
 }
