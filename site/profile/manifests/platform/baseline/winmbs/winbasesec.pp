6832016103d391eb0a1043f793edb2848a6f6c48
Greg Loughmiller <greg.loughmiller@greg.loughmiller-C02M54SNFD57>
2017-10-23 10:45:34 -0400

    parameters v13

--- site/profile/manifests/platform/baseline/winmbs/winbasesec.pp
+++ site/profile/manifests/platform/baseline/winmbs/winbasesec.pp
@@ -1,15 +1,12 @@
-olass profile::platform::baseline::winmbs::winbasesec($mymessage) {
-#
-#
-#### $mymessage="The Puppet Agent service periodically manages your configuration and this message is a variable"
-#
-registry_key { 'HKLM\System\CurrentControlSet\Services\Puppet':
+olass profile::platform::baseline::winmbs::winbasesec(String $mymessage='default') {
+
+  registry_key { 'HKLM\System\CurrentControlSet\Services\Puppet':
+      ensure => present,
+  }
+  registry_value { 'HKLM\System\CurrentControlSet\Services\Puppet\Description':
     ensure => present,
-}
-registry_value { 'HKLM\System\CurrentControlSet\Services\Puppet\Description':
-  ensure => present,
-  type   => string,
-  data   => $mymessage,
-}
+    type   => string,
+    data   => $mymessage,
+  }
 
 }
